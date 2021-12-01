ef1d239b921ca72375c1c4dcfea7a465f699e4cb
mgarciate <mgarciate@gmail.com>
2021-12-01 16:16:28 +0100

    Remove beam sync fixing https://github.com/NethermindEth/nethermind/issues/3495

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,8 +5,6 @@ ADVANCED_FILE=/nethermind/configs/mainnet_advanced.cfg
 
 if [ "${MODE}" == "archive" ];then
     EXTRA_OPTS="--config mainnet_archive"
-elif [ "${MODE}" == "beamsync" ];then
-    EXTRA_OPTS="--config mainnet_beam"
 elif [ "${MODE}" == "advanced" ];then
     EXTRA_OPTS="--config mainnet_advanced"
     #Edit selected options
